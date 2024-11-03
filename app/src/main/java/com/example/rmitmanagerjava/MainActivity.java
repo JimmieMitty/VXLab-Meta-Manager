1adcea2f609e21c54906fe35d9005ae2df3c5590
Ian Peake <ian.peake@rmit.edu.au>
2024-11-03 15:53:17 +1100

    untested version using local res/config api_url value to retrieve test.apk, plus install button attempting to install downloaded apk

--- app/src/main/java/com/example/rmitmanagerjava/MainActivity.java
+++ app/src/main/java/com/example/rmitmanagerjava/MainActivity.java
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
@@ -44,7 +46,9 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Properties;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -59,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
         // Reference the TextView and Button from the layout
         TextView log = findViewById(R.id.operation_log);
         Button downloadButton = findViewById(R.id.Download);
-
         // Set an OnClickListener for the button
         downloadButton.setOnClickListener(new View.OnClickListener() {
             @SuppressLint("SetTextI18n")
@@ -67,7 +70,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Change the text when the button is clicked
                 log.setText("Downloading Files");
-                downloadFile("https://filesampleshub.com/download/document/txt/sample2.txt", "testText.txt", MainActivity.this, Environment.DIRECTORY_DOWNLOADS);
+                String apiUrl = getConfigValue(getApplicationContext(), "api_url");
+                downloadFile(apiUrl+"/test.apk", "test.apk", MainActivity.this, Environment.DIRECTORY_DOWNLOADS);
+            }
+        });
+        Button installButton = findViewById(R.id.Install);
+        // Set an OnClickListener for the button
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                // Change the text when the button is clicked
+                log.setText("Installing Files");
+                try {
+                    installPackage(Environment.DIRECTORY_DOWNLOADS+"test.apk");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
@@ -75,17 +94,13 @@ public class MainActivity extends AppCompatActivity {
     public void downloadFile(String url, String fileName, Context context, String appDirectory) {
         DownloadManager downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
         Uri uri = Uri.parse(url);
-
         DownloadManager.Request request = new DownloadManager.Request(uri);
         request.setTitle("Downloading " + fileName);
         request.setDescription("File is downloading...");
-
         // Set the destination path
         request.setDestinationInExternalPublicDir(appDirectory, fileName);
-
         // Make the notification visible in the status bar
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
-
         // Start the download
         downloadManager.enqueue(request);
     }
@@ -98,30 +113,23 @@ public class MainActivity extends AppCompatActivity {
             ApplicationInfo appInfo = packageInfo.applicationInfo;
             package_name = appInfo.packageName;
         }
-
         if (package_name == null) {
             Toast.makeText(this, "Install Failed : Can't get the package name", Toast.LENGTH_LONG).show();
             return false;
         }
-
         Toast.makeText(getApplicationContext(), "Start Installation", Toast.LENGTH_SHORT).show();
-
         PackageInstaller packageInstaller = getPackageManager().getPackageInstaller();
         PackageInstaller.SessionParams params = new PackageInstaller.SessionParams(
                 PackageInstaller.SessionParams.MODE_FULL_INSTALL);
         params.setAppPackageName(package_name);
-
         // set params
         int sessionId = packageInstaller.createSession(params);
         PackageInstaller.Session session = packageInstaller.openSession(sessionId);
-
         OutputStream out = session.openWrite(package_name, 0, -1);
-
         long sizeBytes = 0;
         File file = new File(apkPath);
         if (file.isFile())
             sizeBytes = file.length();
-
         int total = 0;
         FileInputStream fis = new FileInputStream(apkPath);
         byte[] buffer = new byte[65536];
@@ -133,9 +141,7 @@ public class MainActivity extends AppCompatActivity {
         session.fsync(out);
         fis.close();
         out.close();
-
         session.commit(createIntentSender(getApplicationContext(), sessionId));
-
         return true;
     }
 
@@ -157,5 +163,21 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Install Failed!", Toast.LENGTH_LONG).show();
     }
 
+    private static final String TAG = "Helper";
+
+    public static String getConfigValue(Context context, String name) {
+        Resources resources = context.getResources();
+        try {
+            InputStream rawResource = resources.openRawResource(R.raw.config);
+            Properties properties = new Properties();
+            properties.load(rawResource);
+            return properties.getProperty(name);
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Unable to find the config file: " + e.getMessage());
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to open config file.");
+        }
+        return null;
+    }
 
 }
